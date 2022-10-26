3f895326be043d1baa42c3a7b59c40dad688fb88
cris <zhonghl003@gmail.com>
2022-10-26 11:08:56 +0800

    x

--- cmd/logicalbackup/cmd/root.go
+++ cmd/logicalbackup/cmd/root.go
@@ -3,24 +3,20 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var commonArgs struct {
-	workDir      string
-	threads      int
-	region       string
-	endpointURL  string
-	usePathStyle bool
+	dumpDir string
+	threads int
 }
 
 var mysqlPassword = os.Getenv("MYSQL_PASSWORD")
 var rootCmd = &cobra.Command{
 	Use:     "mybackup",
-	Version: "v2.1.1",
+	Version: "v3.0.0",
 	Short:   "backup and restore MySQL data",
 	Long:    "Backup and restore MySQL data.",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
@@ -29,15 +25,8 @@ var rootCmd = &cobra.Command{
 		if len(mysqlPassword) == 0 {
 			return errors.New("no MYSQL_PASSWORD environment variable")
 		}
-		if len(commonArgs.endpointURL) > 0 {
-			_, err := url.Parse(commonArgs.endpointURL)
-			if err != nil {
-				return fmt.Errorf("invalid endpoint URL %s: %w", commonArgs.endpointURL, err)
-			}
-		}
-
 		// mysqlsh command creates some files in $HOME.
-		os.Setenv("HOME", commonArgs.workDir)
+		os.Setenv("HOME", commonArgs.dumpDir)
 		return nil
 	},
 }
@@ -50,9 +39,6 @@ func Execute() {
 }
 func init() {
 	pf := rootCmd.PersistentFlags()
-	pf.StringVar(&commonArgs.workDir, "work-dir", "/work", "The writable working directory")
+	pf.StringVar(&commonArgs.dumpDir, "work-dir", "/work", "The writable working directory")
 	pf.IntVar(&commonArgs.threads, "threads", 4, "The number of threads to be used")
-	pf.StringVar(&commonArgs.region, "region", "", "AWS region")
-	pf.StringVar(&commonArgs.endpointURL, "endpoint", "", "S3 API endpoint URL")
-	pf.BoolVar(&commonArgs.usePathStyle, "use-path-style", false, "Use path-style S3 API")
 }
